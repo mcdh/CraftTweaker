0fe149e039392db2d30156d1a2d4ab0bf0225024
Jared <jaredlll08@gmail.com>
2019-07-05 05:30:17 +0200

    Adding this class breaks old scripts
    ```
    recipes.addShapeless("shapedtest", <item:minecraft:diamond> * 9, [<item:minecraft:dirt>], (usualOut as IItemStack, inputs as IItemStack[]) => {
    if(inputs[0].displayName == "totally real diamond block" ){
        return usualOut;
    }
    return <item:minecraft:clay>.setDisplayName("Diamond");
    });
    
    ```
    that worked fine before this commit, after this commit
    
    ```
    [05:27:52.592][CONSTRUCT][SERVER][ERROR] ERROR test.zs:12:0: No compatible methods found: 
    Parameter 2: cannot cast IItemStack[]`unique to IIngredient[]
    ```

---
+++ src/main/java/com/blamejared/crafttweaker/impl/managers/CTFurnaceManager.java
@@ -0,0 +1,43 @@
+package com.blamejared.crafttweaker.impl.managers;
+
+import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker.api.item.IIngredient;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.api.managers.ISingleRecipeManager;
+import net.minecraft.item.crafting.IRecipeType;
+import org.openzen.zencode.java.ZenCodeGlobals;
+import org.openzen.zencode.java.ZenCodeType;
+
+@ZenRegister
+@ZenCodeType.Name("crafttweaker.api.CTFurnaceManager")
+public class CTFurnaceManager implements ISingleRecipeManager {
+    
+    @ZenCodeGlobals.Global("furnace")
+    public static final CTFurnaceManager INSTANCE = new CTFurnaceManager();
+    
+    @Override
+    public void addRecipe(String name, IItemStack output, IIngredient input, float xp, int cookTime, @ZenCodeType.Optional RecipeFunctionSingle function) {
+//        CraftTweakerAPI.apply(new ActionAddRecipe(getRecipeType(), new CTFurnaceRecipe(new ResourceLocation(CraftTweaker.MODID, name), input.asVanillaIngredient(), output.getInternal(), xp, cookTime, function), ""));
+    }
+    
+    @Override
+    public void removeRecipe(IItemStack output, IIngredient input) {
+    
+    }
+    
+    @Override
+    public void removeByName(String name) {
+    
+    }
+    
+    @Override
+    public void remove(IItemStack output) {
+    
+    }
+    
+    @Override
+    public IRecipeType getRecipeType() {
+        return IRecipeType.SMELTING;
+    }
+    
+}
